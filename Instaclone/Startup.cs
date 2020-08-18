10c7bf904729d0b5d4acc51df97e0608acb60c76
sshimabuku <shimasstrafez@gmail.com>
2020-08-17 23:13:07 -0400

    Initial setup

---
+++ Instaclone/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Instaclone.Startup))]
+namespace Instaclone
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
