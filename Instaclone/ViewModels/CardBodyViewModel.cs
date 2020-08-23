f8a9081efd71fd5afe05228f939971e33b429f1c
sshimabuku <shimasstrafez@gmail.com>
2020-08-23 00:06:51 -0400

    Added create comment form

---
+++ Instaclone/ViewModels/CardBodyViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Instaclone.Models;
+
+namespace Instaclone.ViewModels
+{
+    public class CardBodyViewModel
+    {
+        public Post Post { get; set; }
+        public Comment Comment { get; set; }
+    }
+}
